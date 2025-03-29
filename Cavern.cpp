64624e5c8ea42c7b6ad46d5a7ed27e4db5fe03f0
alfr0x800 <199596498+alfr0x800@users.noreply.github.com>
2025-03-29 23:54:55 +0000

    Start of command support

--- Cavern.cpp
+++ Cavern.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <iostream>
 #include <print>
 #include <random>
 #include <string_view>
@@ -21,10 +22,16 @@ private:
         StonePickaxe, IronPickaxe, DiamondPickaxe, RubyPickaxe, EmeraldPickaxe,
         // Weapons
         StoneSword, IronSword, DiamondSword, GemstoneSlicer, NuclearBlaster,
-        // Serpent
-        Serpent,
+        // Entities
+        Player, Serpent,
         // Count
         Count
+    };
+
+    enum Command
+    {
+        MoveNorth = 'n', MoveEast = 'e', MoveSouth = 's', MoveWest = 'w', Mine ='m', Attack = 'a',
+        Open = 'o', Help = 'h'
     };
 
 private:
@@ -35,42 +42,82 @@ private:
     // Table for converting the enum values into strings
     std::array<std::string_view, Item::Count> m_itemStringTable
     {
-        "-", "Wood", "Log", "Stick", "Apple", "Cracker", "Bread", "Potato", "Jacket potato", 
-        "Carrot", "Mango", "#", "Coal", "Iron", "Gold", "Diamond", "Ruby", "Emerald", "Lapiz", 
+        "Air", "Wood", "Log", "Stick", "Apple", "Cracker", "Bread", "Potato", "Jacket potato", 
+        "Carrot", "Mango", "Stone", "Coal", "Iron", "Gold", "Diamond", "Ruby", "Emerald", "Lapiz", 
         "Topaz", "Amethyst", "Uranium", "Thorium", "Stone pickaxe", "Iron pickaxe", 
         "Diamond pickaxe", "Ruby pickaxe", "Emerald pickaxe", "Stone sword", "Iron sword",
-        "Diamond sword", "Gemstone slicer", "Nuclear blaster", "§"
+        "Diamond sword", "Gemstone slicer", "Nuclear blaster", "Player", "Serpent"
     };
 
     // Cave and depth
     std::array<std::array<Item, 16>, 16> m_cave{};
     unsigned depth{};
     // Player information
+    unsigned m_x{};
+    unsigned m_y{};
     std::unordered_map<Item, unsigned> m_inventory{};
     unsigned m_health{ s_maxHealth };
     unsigned m_hunger{ s_maxHunger };
+    bool m_inSerpentFight{};
     // The number of turns from the start
-    unsigned m_turns{};
+    unsigned m_turns{ 10 };
 
 public:
     Cavern();
 
+    void Play();
+
 private:
+    void GetPlayerResponse();
     void GenerateCave();
-    void GenerateMonster();
+    void GenerateSerpent();
     void GenerateMinerals();
+    void UpdatePlayer();
     void PrintCave();
     void PrintPlayerInfo();
 };
 
 Cavern::Cavern()
 {
-    m_inventory[Item::Apple] = 32;
-    m_inventory[Item::Uranium] = 17;
-    m_inventory[Item::Stick] = 2;
     GenerateCave();
-    PrintPlayerInfo();
-    PrintCave();
+}
+
+void Cavern::Play()
+{
+    while (true)
+    {
+        PrintCave();
+        PrintPlayerInfo();
+        GetPlayerResponse();
+        UpdatePlayer();
+    }
+}
+
+void Cavern::GetPlayerResponse()
+{
+    // Get the user input
+    std::cout << "> ";
+    std::string input;
+    std::cin >> input;
+
+    // Run command
+    switch (std::tolower(input[0]))
+    {
+    case Command::MoveNorth: break;
+    case Command::MoveSouth: break;
+    case Command::MoveWest: break;
+    case Command::MoveEast: break;
+    case Command::Mine: break;
+    case Command::Attack: break;
+    case Command::Open: break;
+    case Command::Help: break;
+    default:
+        std::cout << "Bad command! use 'h' for help" << std::endl;
+        GetPlayerResponse();
+    }
+
+    // Increase the amount of turns
+    m_turns++;
 }
 
 void Cavern::GenerateCave()
@@ -102,9 +149,9 @@ void Cavern::GenerateCave()
         for (int i{}; i < airSpace && i + off < rowSz - 1; i++)
             row[i + off] = Item::Air;
 
-        // Every 10 depth a monster will have to be defeated
+        // Every 10 depth a serpent will have to be defeated
         if (depth % 10 == 0)
-            GenerateMonster();
+            GenerateSerpent();
         else
             GenerateMinerals();
 
@@ -114,35 +161,60 @@ void Cavern::GenerateCave()
     }
 }
 
-void Cavern::GenerateMonster()
+void Cavern::GenerateSerpent()
 {
-
+    m_cave[3][11] = Item::Serpent;
+    m_cave[5][11] = Item::Player;
+    m_inSerpentFight = true;
 }
 
 void Cavern::GenerateMinerals()
 {}
+
+void Cavern::UpdatePlayer()
+{
+    // Start to die if there is no hunger and every 12 turns the hunger goes down
+    if (m_hunger == 0)
+        m_health--;
+    else if (m_turns % 12 == 0)
+        m_hunger--;
+}
 
 void Cavern::PrintCave()
 {
     for (const auto& row : m_cave)
     {
         for (const auto& col : row)
-            std::print("{}", m_itemStringTable[col][0]);
-        std::print("\n");
+            switch (col)
+            {
+            case Item::Air:
+                std::cout << "-";
+                break;
+            case Item::Stone:
+                std::cout << "#";
+                break;
+            case Item::Player:
+                std::cout << "\x1b[1;32m☺︎\x1b[0m";
+                break;
+            case Item::Serpent:
+                std::cout << "\x1b[1;31m§\x1b[0m";
+                break;
+            default:
+                std::cout << m_itemStringTable[col][0];
+                break;
+            }
+        std::cout << std::endl;
     }
 }
 
 void Cavern::PrintPlayerInfo()
 {
-    // Print the player stats
-    std::println("Health: {}/{}", m_health, s_maxHealth);
-    std::println("Hunger: {}/{}", m_hunger, s_maxHunger);
-    // Print the inventory
-    for (const auto&[item, amount] : m_inventory)
-        std::println("{}x {}", amount, m_itemStringTable[item]);
+    std::cout << "Health: " << m_health << "/" << s_maxHealth << std::endl;
+    std::cout << "Hunger: " << m_hunger << "/" << s_maxHunger << std::endl;
 }
 
 int main()
 {
     Cavern cavern;
+    cavern.Play();
 }
